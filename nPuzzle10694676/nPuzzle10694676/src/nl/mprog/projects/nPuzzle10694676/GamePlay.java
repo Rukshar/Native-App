d2800fa542a96bd7539b25d259c9e2885f57a588
Rukshar <ruky_wh@hotmail.com>
2014-11-20 19:21:15 +0100

    win scherm toegevoegd en menu in gameplay activity toegevoegd

--- nPuzzle10694676/nPuzzle10694676/src/nl/mprog/projects/nPuzzle10694676/GamePlay.java
+++ nPuzzle10694676/nPuzzle10694676/src/nl/mprog/projects/nPuzzle10694676/GamePlay.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 
 public class GamePlay extends Activity {
@@ -15,7 +18,7 @@ public class GamePlay extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.gameplay);
 		Intent intent = getIntent();
-		int position = (Integer) intent.getExtras().get("id");
+		final int position = (Integer) intent.getExtras().get("id");
 		
 		try{
 		
@@ -26,14 +29,38 @@ public class GamePlay extends Activity {
 			
 			ImageView iv = (ImageView) findViewById(R.id.imageView1);
 			iv.setImageBitmap(cropped);
+			iv.setClickable(true);
+			iv.setOnClickListener(new View.OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+					Intent i = new Intent(getApplicationContext(), YouWin.class);
+					i.putExtra("Image", position);
+					startActivity(i);
+				}
+			});
 		}
 		catch (OutOfMemoryError e){
 			
 		}
 
-
 	}
-	
-	
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		getMenuInflater().inflate(R.menu.image_selection, menu);
+        return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		   int id = item.getItemId();
+	       if (id == R.id.action_settings) {
+	            return true;
+	       }
+	       return super.onOptionsItemSelected(item);
+	}
 }
