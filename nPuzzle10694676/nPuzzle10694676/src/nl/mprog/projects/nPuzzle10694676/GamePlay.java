4bdb595c6da734dd79a1b368faf0fcb9e8b250b9
Rukshar <ruky_wh@hotmail.com>
2014-11-28 02:08:13 +0100

    schuiven werkt

--- nPuzzle10694676/nPuzzle10694676/src/nl/mprog/projects/nPuzzle10694676/GamePlay.java
+++ nPuzzle10694676/nPuzzle10694676/src/nl/mprog/projects/nPuzzle10694676/GamePlay.java
@@ -1,6 +1,7 @@
 package nl.mprog.projects.nPuzzle10694676;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -9,9 +10,15 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 
 public class GamePlay extends Activity {
+	public static int height;
+	public static int width;
+	public static int columns;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,35 +27,68 @@ public class GamePlay extends Activity {
 		setContentView(R.layout.gameplay);
 		Intent intent = getIntent();
 		final int position = (Integer) intent.getExtras().get("id");
-		ArrayList<Bitmap> crops = new ArrayList<Bitmap>();
+		final ArrayList<Bitmap> crops = new ArrayList<Bitmap>();
 		
 		try{
-					
-			for(int i = 0; i < 300; i += 100){
-				for(int j = 0; j < 300; j +=100){
-					Bitmap background = BitmapFactory.decodeResource(this.getResources(), GridAdapter.puzzles[position]);
-					Bitmap cropped = Bitmap.createBitmap(background, i, j, 200, 200);
-					background.recycle();
-					crops.add(cropped);
+			Bitmap background = BitmapFactory.decodeResource(this.getResources(), GridAdapter.puzzles[position]);
+			Bitmap empty = BitmapFactory.decodeResource(this.getResources(), R.drawable.square);
+			height = background.getHeight();
+			width =  background.getWidth();
+			columns = 3;
+			//System.out.println(height);
+			//System.out.println(width);
+			//System.out.println(empty.getHeight());
+			//System.out.println(empty.getWidth());
+			
+			for(int i = 0; i <= (width - (width/columns)); i += (width/columns)){
+				for(int j = 0; j <= (height - (height/columns)); j += (height/columns)){
+					//System.out.println(i);
+					//System.out.println(j);
+					Bitmap cropped = Bitmap.createBitmap(background, i, j, (width/columns), (height/columns));
+					crops.add(cropped);	
 				}
 			}
-			
-			PlayAdapter playAdapter = new PlayAdapter(getApplicationContext(), crops);
+			Bitmap croppedEmpty = Bitmap.createBitmap(empty, (width/columns)*(columns-1), (height/columns)*(columns-1), 500, 500);
+			background.recycle();
+			empty.recycle();
+			crops.remove(crops.size()-1);
+			crops.add(croppedEmpty);
+			//System.out.println(crops);
+			final PlayAdapter playAdapter = new PlayAdapter(getApplicationContext(), crops);
 			
 			GridView gvp = (GridView) findViewById(R.id.gridviewplay);
+			gvp.setNumColumns(columns);
 			gvp.setAdapter(playAdapter);
-			//gvp.setImageBitmap(cropped);
-			//gvp.setClickable(true);
-			//gvp.setOnClickListener(new View.OnClickListener() {
-				
-				//@Override
-				//public void onClick(View v) {
+			final Bitmap emptyTile = crops.get(crops.size()-1);
+			gvp.setOnItemClickListener(new OnItemClickListener(){
+
+				@Override
+				public void onItemClick(AdapterView<?> parent, View view,
+						int position, long id) {
 					// TODO Auto-generated method stub
-					//Intent i = new Intent(getApplicationContext(), YouWin.class);
-					//i.putExtra("Image", position);
-					//startActivity(i);
-				//}
-			//});
+					int emptySpot = crops.indexOf(emptyTile);
+					if(position >= 0 && position < crops.size()){
+						if(position -1 == emptySpot){
+							Collections.swap(crops, position, position-1);
+							playAdapter.notifyDataSetChanged();
+						}
+						else if(position + 1 == emptySpot && (position+1) % columns != 0){
+							Collections.swap(crops, position, position+1);
+							playAdapter.notifyDataSetChanged();
+						}
+						else if(position - columns == emptySpot){
+							Collections.swap(crops, position, position-columns);
+							playAdapter.notifyDataSetChanged();
+						}
+						else if(position + columns == emptySpot){
+							Collections.swap(crops, position, position+columns);
+							playAdapter.notifyDataSetChanged();
+						}
+						System.out.println();
+					}
+				}
+				
+			});
 		}
 		catch (OutOfMemoryError e){
 			
