80a4e9f0d2b41d52d5c5a37e1a39635da1990beb
Rukshar <ruky_wh@hotmail.com>
2014-12-11 14:58:09 +0100

    BUG

---
+++ nPuzzle10694676/nPuzzle10694676/src/nl/mprog/projects/nPuzzle10694676/GamePlay2.java
@@ -0,0 +1,19 @@
+package nl.mprog.projects.nPuzzle10694676;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class GamePlay2 extends Activity {
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);	
+		
+	}
+	
+}
+
